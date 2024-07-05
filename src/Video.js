2e4de31310bf159be8c0be3b2c3798b94949b208
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2024-07-05 22:50:07 +0200

    video page changes

--- src/Video.js
+++ src/Video.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import {NavLink, useLocation} from 'react-router-dom';
 import axios from 'axios';
 import { format } from "date-fns";
 import './css/main.css';
@@ -15,7 +15,9 @@ import Footer from "./components/Footer";
 function Video() {
     const location = useLocation();
     const [videoDetails, setVideoDetails] = useState(null);
+    const [subscriberCount, setSubscriberCount] = useState(null);
     const [error, setError] = useState('');
+    const [isSubscribed, setIsSubscribed] = useState(false);
 
     useEffect(() => {
         const fetchVideoData = async () => {
@@ -42,6 +44,15 @@ function Video() {
                 // Then fetch video details
                 const videoResponse = await axios.get(`http://localhost:5000/videos/${encodeURIComponent(videoUrl)}`, { headers });
                 setVideoDetails(videoResponse.data);
+                const videoData = videoResponse.data;
+
+                // Fetch subscription status
+                const subscriptionStatus = await axios.get(`http://localhost:5000/subscribe/status/${encodeURIComponent(videoData.creator.name)}`, { headers });
+                setIsSubscribed(subscriptionStatus.data.isSubscribed);
+
+                // Fetch subscriber count
+                const subscriberResponse = await axios.get(`http://localhost:5000/user/${encodeURIComponent(videoData.creator.name)}/subscribers`, { headers });
+                setSubscriberCount(subscriberResponse.data.subscriberCount);
 
             } catch (err) {
                 console.error('Error fetching video data:', err);
@@ -57,8 +68,31 @@ function Video() {
     }
 
     if (!videoDetails) {
-        return <div>Loading...</div>;
+        return <section className="loading">
+            <div className="loading-box"><p className="loading-box-text">Loading</p></div>
+        </section>;
     }
+
+    const handleSubscribe = async () => {
+        try {
+            const endpoint = isSubscribed ? `unsubscribe` : `subscribe`;
+            const response = await axios.post(`http://localhost:5000/subscribe/${encodeURIComponent(videoDetails.creator.name)}`, {}, {
+                headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+            });
+
+            // Check response and update state accordingly
+            if (response.data.message === 'Subscribed successfully') {
+                setIsSubscribed(true); // Update to subscribed
+            } else if (response.data.message === 'Unsubscribed successfully') {
+                setIsSubscribed(false); // Update to unsubscribed
+            }
+        } catch (err) {
+            setError('Failed to subscribe');
+        }
+    };
+
+    const profilePictureUrl = `http://localhost:5000/${videoDetails.creator.profilePicture}`;
+    const bannerUrl = `http://localhost:5000/${videoDetails.creator.banner}`;
 
     return (
         <>
@@ -77,54 +111,72 @@ function Video() {
                     <div className="video-inner-right">
                         <div className="video-inner-right-box">
                             <div className="video-inner-right-box-top">
-                                <h3 className="video-inner-right-box-top-title">{videoDetails.title}</h3>
-                                <p className="video-inner-right-box-top-subtitle">{videoDetails.views} Views &middot; {format(new Date(videoDetails.datePosted), 'MMM d, yyyy')}</p>
+                                <div className="video-inner-right-box-top-user">
+                                    <NavLink to={`/profile/${videoDetails.creator.name}`} className="video-inner-right-box-top-user-inner">
+                                        <div className="video-inner-right-box-top-user-container">
+                                            <img className="video-inner-right-box-top-user-container-image"
+                                                 src={profilePictureUrl}
+                                                 alt=""/>
+                                        </div>
+                                        <div className="video-inner-right-box-top-user-text">
+                                            <h3 className="video-inner-right-box-top-user-text-username">
+                                                <span>{videoDetails.creator.name}</span>
+                                                <svg className="verified-icon" viewBox="0 0 22 22" aria-hidden="true">
+                                                    <g>
+                                                        <linearGradient gradientUnits="userSpaceOnUse" id="a" x1="4.411"
+                                                                        x2="18.083"
+                                                                        y1="2.495" y2="21.508">
+                                                            <stop offset="0"></stop>
+                                                            <stop offset=".539"></stop>
+                                                            <stop offset=".68"></stop>
+                                                            <stop offset="1"></stop>
+                                                            <stop offset="1"></stop>
+                                                        </linearGradient>
+                                                        <linearGradient gradientUnits="userSpaceOnUse" id="b" x1="5.355"
+                                                                        x2="16.361"
+                                                                        y1="3.395" y2="19.133">
+                                                            <stop offset="0"></stop>
+                                                            <stop offset=".406"></stop>
+                                                            <stop offset=".989"></stop>
+                                                        </linearGradient>
+                                                        <g clip-rule="evenodd" fill-rule="evenodd">
+                                                            <path
+                                                                d="M13.324 3.848L11 1.6 8.676 3.848l-3.201-.453-.559 3.184L2.06 8.095 3.48 11l-1.42 2.904 2.856 1.516.559 3.184 3.201-.452L11 20.4l2.324-2.248 3.201.452.559-3.184 2.856-1.516L18.52 11l1.42-2.905-2.856-1.516-.559-3.184zm-7.09 7.575l3.428 3.428 5.683-6.206-1.347-1.247-4.4 4.795-2.072-2.072z"></path>
+                                                            <path
+                                                                d="M13.101 4.533L11 2.5 8.899 4.533l-2.895-.41-.505 2.88-2.583 1.37L4.2 11l-1.284 2.627 2.583 1.37.505 2.88 2.895-.41L11 19.5l2.101-2.033 2.895.41.505-2.88 2.583-1.37L17.8 11l1.284-2.627-2.583-1.37-.505-2.88zm-6.868 6.89l3.429 3.428 5.683-6.206-1.347-1.247-4.4 4.795-2.072-2.072z"></path>
+                                                            <path
+                                                                d="M6.233 11.423l3.429 3.428 5.65-6.17.038-.033-.005 1.398-5.683 6.206-3.429-3.429-.003-1.405.005.003z"></path>
+                                                        </g>
+                                                    </g>
+                                                </svg>
+                                            </h3>
+                                            <p className="video-inner-right-box-top-user-text-date">{subscriberCount} Subscribers</p>
+                                        </div>
+                                    </NavLink>
+                                    <div className="video-inner-right-box-top-container">
+                                        {isSubscribed ? (
+                                            <button className="blackbutton" onClick={handleSubscribe}
+                                                    disabled={isSubscribed}>
+                                                Subscribed
+                                                <div className="blackbutton-shadow"></div>
+                                            </button>
+                                        ) : (
+                                            <button className="mainbutton" onClick={handleSubscribe}
+                                                    disabled={isSubscribed}>
+                                                Subscribe
+                                            </button>
+                                        )}
+                                    </div>
+                                    <div className="video-inner-right-box-top-user-overlay"></div>
+                                    <img className="video-inner-right-box-top-user-background"
+                                         src={bannerUrl}
+                                         alt=""/>
+                                </div>
                             </div>
                             <div className="video-inner-right-box-center">
-                                <div className="video-inner-right-box-center-user">
-                                    <div className="video-inner-right-box-center-user-container">
-                                        <img className="video-inner-right-box-center-user-container-image"
-                                             src="https://images.unsplash.com/photo-1595258545564-bffdf78be46c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                             alt=""/>
-                                    </div>
-                                    <div className="video-inner-right-box-center-user-text">
-                                        <h3 className="video-inner-right-box-center-user-text-username">
-                                            <span>Channel Name</span>
-                                            <svg className="verified-icon" viewBox="0 0 22 22" aria-hidden="true">
-                                                <g>
-                                                    <linearGradient gradientUnits="userSpaceOnUse" id="a" x1="4.411"
-                                                                    x2="18.083"
-                                                                    y1="2.495" y2="21.508">
-                                                        <stop offset="0"></stop>
-                                                        <stop offset=".539"></stop>
-                                                        <stop offset=".68"></stop>
-                                                        <stop offset="1"></stop>
-                                                        <stop offset="1"></stop>
-                                                    </linearGradient>
-                                                    <linearGradient gradientUnits="userSpaceOnUse" id="b" x1="5.355"
-                                                                    x2="16.361"
-                                                                    y1="3.395" y2="19.133">
-                                                        <stop offset="0"></stop>
-                                                        <stop offset=".406"></stop>
-                                                        <stop offset=".989"></stop>
-                                                    </linearGradient>
-                                                    <g clip-rule="evenodd" fill-rule="evenodd">
-                                                        <path
-                                                            d="M13.324 3.848L11 1.6 8.676 3.848l-3.201-.453-.559 3.184L2.06 8.095 3.48 11l-1.42 2.904 2.856 1.516.559 3.184 3.201-.452L11 20.4l2.324-2.248 3.201.452.559-3.184 2.856-1.516L18.52 11l1.42-2.905-2.856-1.516-.559-3.184zm-7.09 7.575l3.428 3.428 5.683-6.206-1.347-1.247-4.4 4.795-2.072-2.072z"></path>
-                                                        <path
-                                                            d="M13.101 4.533L11 2.5 8.899 4.533l-2.895-.41-.505 2.88-2.583 1.37L4.2 11l-1.284 2.627 2.583 1.37.505 2.88 2.895-.41L11 19.5l2.101-2.033 2.895.41.505-2.88 2.583-1.37L17.8 11l1.284-2.627-2.583-1.37-.505-2.88zm-6.868 6.89l3.429 3.428 5.683-6.206-1.347-1.247-4.4 4.795-2.072-2.072z"></path>
-                                                        <path
-                                                            d="M6.233 11.423l3.429 3.428 5.65-6.17.038-.033-.005 1.398-5.683 6.206-3.429-3.429-.003-1.405.005.003z"></path>
-                                                    </g>
-                                                </g>
-                                            </svg>
-                                        </h3>
-                                        <p className="video-inner-right-box-center-user-text-date">15M Subscribers</p>
-                                    </div>
-                                </div>
-                                <div className="video-inner-right-box-center-container">
-                                    <button className="mainbutton">Subscribe</button>
-                                </div>
+                                <h3 className="video-inner-right-box-center-title">{videoDetails.title}</h3>
+                                <p className="video-inner-right-box-center-subtitle">{videoDetails.views} Views &middot; {format(new Date(videoDetails.datePosted), 'MMM d, yyyy')}</p>
+                                <p className="video-inner-right-box-center-description">{videoDetails.description}</p>
                             </div>
                             <div className="video-inner-right-box-bottom">
                                 <div className="video-inner-right-box-bottom-item">
@@ -177,7 +229,8 @@ function Video() {
                                         <title>Folder</title>
                                         <g id="folder" stroke="none" stroke-width="1" fill="none"
                                            fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-                                            <g id="folder-inner" transform="translate(2.500000, 2.500000)" stroke="#000000"
+                                            <g id="folder-inner" transform="translate(2.500000, 2.500000)"
+                                               stroke="#000000"
                                                stroke-width="1.5">
                                                 <line x1="4.8057" y1="12.0742685" x2="14.3987" y2="12.0742685"
                                                       id="Stroke-1"/>
@@ -194,7 +247,8 @@ function Video() {
                                         <title>Download</title>
                                         <g id="download" stroke="none" stroke-width="1" fill="none"
                                            fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-                                            <g id="download-inner" transform="translate(2.000000, 2.000000)" stroke="#000000"
+                                            <g id="download-inner" transform="translate(2.000000, 2.000000)"
+                                               stroke="#000000"
                                                stroke-width="1.5">
                                                 <line x1="9.8791" y1="12.791" x2="9.8791" y2="0.75" id="Stroke-1"/>
                                                 <polyline id="Stroke-3"
@@ -209,17 +263,17 @@ function Video() {
                             </div>
                             <div className="video-inner-right-box-shadow"></div>
                         </div>
-                        <div className="video-inner-right-box">
-                            <div className="video-inner-right-box-top">
-                                <h3 className="video-inner-right-box-top-title">Comments</h3>
+                        <div className="video-inner-right-box-comments">
+                            <div className="video-inner-right-box-comments-top">
+                                <h3 className="video-inner-right-box-comments-top-title">Comments</h3>
                             </div>
-                            <div className="video-inner-right-box-center">
+                            <div className="video-inner-right-box-comments-center">
 
                             </div>
-                            <div className="video-inner-right-box-bottom">
+                            <div className="video-inner-right-box-comments-bottom">
 
                             </div>
-                            <div className="video-inner-right-box-shadow"></div>
+                            <div className="video-inner-right-box-comments-shadow"></div>
                         </div>
                     </div>
                 </div>
