104b15858f304800825f76b9c5c5fcd332a89d2b
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2024-07-24 16:19:35 +0200

    profile menu styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -370,7 +370,89 @@ const Navbar = ({ profile }) => {
                         </div>
                         <div className="navbar-inner-right-profile-menu-center">
                             <div className="navbar-inner-right-profile-menu-center-left">
-                                <p>Achievements</p>
+                                <NavLink to="/upload" className="navbar-inner-right-profile-menu-center-left-link">
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                         width="24px" height="24px"
+                                         viewBox="0 0 24 24" version="1.1">
+                                        <title>Plus</title>
+                                        <g id="plus" stroke="none" stroke-width="1" fill="none"
+                                           fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                                            <g id="plus-inner" transform="translate(2.300000, 2.300000)"
+                                               stroke="#000000"
+                                               stroke-width="1.5">
+                                                <line x1="9.73684179" y1="6.162632" x2="9.73684179"
+                                                      y2="13.3110531"
+                                                      id="Stroke-1"/>
+                                                <line x1="13.3146315" y1="9.73684179" x2="6.158842"
+                                                      y2="9.73684179"
+                                                      id="Stroke-2"/>
+                                                <path
+                                                    d="M-3.55271368e-14,9.73684211 C-3.55271368e-14,2.43473684 2.43473684,2.13162821e-14 9.73684211,2.13162821e-14 C17.0389474,2.13162821e-14 19.4736842,2.43473684 19.4736842,9.73684211 C19.4736842,17.0389474 17.0389474,19.4736842 9.73684211,19.4736842 C2.43473684,19.4736842 -3.55271368e-14,17.0389474 -3.55271368e-14,9.73684211 Z"
+                                                    id="Stroke-3"/>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </NavLink>
+                                <div className="navbar-inner-right-profile-menu-center-left-link">
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                         width="24px" height="24px"
+                                         viewBox="0 0 24 24" version="1.1">
+                                        <title>Video</title>
+                                        <g id="video" stroke="none" stroke-width="1" fill="none"
+                                           fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                                            <g id="video-inner" transform="translate(2.514381, 5.114095)"
+                                               stroke="#000000"
+                                               stroke-width="1.5">
+                                                <path
+                                                    d="M13.6370476,4.55866688 C15.4751429,3.10152403 17.9418095,1.69200022 18.4084762,2.19676212 C19.1799048,3.02533355 19.1132381,10.9110478 18.4084762,11.6634288 C17.9799048,12.1300955 15.4941905,10.7205716 13.6370476,9.2729526"
+                                                    id="Stroke-1"/>
+                                                <path
+                                                    d="M-6.21724894e-15,6.92285714 C-6.21724894e-15,1.73047619 1.7247619,-2.66453526e-15 6.90095238,-2.66453526e-15 C12.0761905,-2.66453526e-15 13.8009524,1.73047619 13.8009524,6.92285714 C13.8009524,12.1142857 12.0761905,13.8457143 6.90095238,13.8457143 C1.7247619,13.8457143 -6.21724894e-15,12.1142857 -6.21724894e-15,6.92285714 Z"
+                                                    id="Stroke-3"/>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </div>
+                                <div className="navbar-inner-right-profile-menu-center-left-link">
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                         width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <title>Folder</title>
+                                        <g id="folder" stroke="none" stroke-width="1" fill="none"
+                                           fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                                            <g id="folder-inner" transform="translate(2.500000, 2.500000)"
+                                               stroke="#000000"
+                                               stroke-width="1.5">
+                                                <line x1="4.8057" y1="12.0742685" x2="14.3987" y2="12.0742685"
+                                                      id="Stroke-1"/>
+                                                <path
+                                                    d="M-1.13686838e-13,5.29836453 C-1.13686838e-13,2.85645977 1.25,0.75931691 3.622,0.272650243 C5.993,-0.214968804 7.795,-0.0463973758 9.292,0.761221672 C10.79,1.56884072 10.361,2.76122167 11.9,3.63645977 C13.44,4.51265024 15.917,3.19645977 17.535,4.94217405 C19.229,6.7697931 19.2200005,9.57550739 19.2200005,11.3640788 C19.2200005,18.1602693 15.413,18.6993169 9.61,18.6993169 C3.807,18.6993169 -1.13686838e-13,18.2288407 -1.13686838e-13,11.3640788 L-1.13686838e-13,5.29836453 Z"
+                                                    id="Stroke-2"/>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </div>
+                                <div className="navbar-inner-right-profile-menu-center-left-link">
+                                    <div className="navbar-inner-right-profile-menu-center-left-link-bigdot">
+                                        <span>3</span>
+                                    </div>
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                         width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                        <title>Notification</title>
+                                        <g id="notification" stroke="none" stroke-width="1" fill="none"
+                                           fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                                            <g id="notification-inner" transform="translate(4.614552, 2.514190)"
+                                               stroke="#000000"
+                                               stroke-width="1.5">
+                                                <path
+                                                    d="M7.38163814,2.84217094e-14 C2.94735243,2.84217094e-14 1.02068576,4.0152381 1.02068576,6.66952381 C1.02068576,8.65333333 1.30830481,8.06952381 0.210209572,10.4895238 C-1.13074281,13.9380952 4.26163814,15.347619 7.38163814,15.347619 C10.5006858,15.347619 15.8930667,13.9380952 14.5530667,10.4895238 C13.4549715,8.06952381 13.7425905,8.65333333 13.7425905,6.66952381 C13.7425905,4.0152381 11.8149715,2.84217094e-14 7.38163814,2.84217094e-14 Z"
+                                                    id="Stroke-1"/>
+                                                <path
+                                                    d="M9.691448,17.998 C8.39716229,19.4437143 6.37811467,19.4608571 5.071448,17.998"
+                                                    id="Stroke-3"/>
+                                            </g>
+                                        </g>
+                                    </svg>
+                                </div>
                             </div>
                             <div className="navbar-inner-right-profile-menu-center-right">
                                 <div className="navbar-inner-right-profile-menu-center-right-filter">
@@ -405,75 +487,7 @@ const Navbar = ({ profile }) => {
                             </div>
                         </div>
                         <div className="navbar-inner-right-profile-menu-bottom">
-                            <ul className="navbar-inner-right-profile-menu-bottom-list">
-                            <NavLink to="/upload" className="navbar-inner-right-profile-menu-bottom-list-item">
-                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                         width="24px" height="24px"
-                                         viewBox="0 0 24 24" version="1.1">
-                                        <title>Plus</title>
-                                        <g id="plus" stroke="none" stroke-width="1" fill="none"
-                                           fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-                                            <g id="plus-inner" transform="translate(2.300000, 2.300000)"
-                                               stroke="#000000"
-                                               stroke-width="1.5">
-                                                <line x1="9.73684179" y1="6.162632" x2="9.73684179"
-                                                      y2="13.3110531"
-                                                      id="Stroke-1"/>
-                                                <line x1="13.3146315" y1="9.73684179" x2="6.158842"
-                                                      y2="9.73684179"
-                                                      id="Stroke-2"/>
-                                                <path
-                                                    d="M-3.55271368e-14,9.73684211 C-3.55271368e-14,2.43473684 2.43473684,2.13162821e-14 9.73684211,2.13162821e-14 C17.0389474,2.13162821e-14 19.4736842,2.43473684 19.4736842,9.73684211 C19.4736842,17.0389474 17.0389474,19.4736842 9.73684211,19.4736842 C2.43473684,19.4736842 -3.55271368e-14,17.0389474 -3.55271368e-14,9.73684211 Z"
-                                                    id="Stroke-3"/>
-                                            </g>
-                                        </g>
-                                    </svg>
-                                    <span>Upload</span>
-                                </NavLink>
-                                <li className="navbar-inner-right-profile-menu-bottom-list-item">
-                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                         width="24px" height="24px"
-                                         viewBox="0 0 24 24" version="1.1">
-                                        <title>Image 2</title>
-                                        <g id="image-2" stroke="none" stroke-width="1" fill="none"
-                                           fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-                                            <g id="image-2-inner" transform="translate(2.000000, 2.000000)"
-                                               stroke="#000000" stroke-width="1.5">
-                                                <path
-                                                    d="M0.75,10.0001 C0.75,16.9371 3.063,19.2501 10,19.2501 C16.937,19.2501 19.25,16.9371 19.25,10.0001 C19.25,3.0631 16.937,0.7501 10,0.7501 C3.063,0.7501 0.75,3.0631 0.75,10.0001 Z"
-                                                    id="Stroke-1"/>
-                                                <path
-                                                    d="M8.5986,6.7842 C8.5986,7.7572 7.8106,8.5452 6.8376,8.5452 C5.8656,8.5452 5.0766,7.7572 5.0766,6.7842 C5.0766,5.8112 5.8656,5.0232 6.8376,5.0232 C7.8106,5.0232 8.5986,5.8112 8.5986,6.7842 Z"
-                                                    id="Stroke-3"/>
-                                                <path
-                                                    d="M19.1201,12.6666 C18.2391,11.7606 16.9931,9.9296 14.7041,9.9296 C12.4151,9.9296 12.3651,13.9676 10.0291,13.9676 C7.6921,13.9676 6.7511,12.5966 5.2281,13.3126 C3.7061,14.0276 2.4661,16.8736 2.4661,16.8736"
-                                                    id="Stroke-5"/>
-                                            </g>
-                                        </g>
-                                    </svg>
-                                    <span>Upload</span>
-                                </li>
-                                <li className="navbar-inner-right-profile-menu-bottom-list-item">
-                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                         width="24px" height="24px"
-                                         viewBox="0 0 24 24" version="1.1">
-                                        <title>Video</title>
-                                        <g id="video" stroke="none" stroke-width="1" fill="none"
-                                           fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
-                                            <g id="video-inner" transform="translate(2.514381, 5.114095)"
-                                               stroke="#000000"
-                                               stroke-width="1.5">
-                                                <path
-                                                    d="M13.6370476,4.55866688 C15.4751429,3.10152403 17.9418095,1.69200022 18.4084762,2.19676212 C19.1799048,3.02533355 19.1132381,10.9110478 18.4084762,11.6634288 C17.9799048,12.1300955 15.4941905,10.7205716 13.6370476,9.2729526"
-                                                    id="Stroke-1"/>
-                                                <path
-                                                    d="M-6.21724894e-15,6.92285714 C-6.21724894e-15,1.73047619 1.7247619,-2.66453526e-15 6.90095238,-2.66453526e-15 C12.0761905,-2.66453526e-15 13.8009524,1.73047619 13.8009524,6.92285714 C13.8009524,12.1142857 12.0761905,13.8457143 6.90095238,13.8457143 C1.7247619,13.8457143 -6.21724894e-15,12.1142857 -6.21724894e-15,6.92285714 Z"
-                                                    id="Stroke-3"/>
-                                            </g>
-                                        </g>
-                                    </svg>
-                                    <span>Go Live</span>
-                                </li>
+                            <ul className="navbar-inner-right-profile-menu-bottom-list list-2">
                                 <li className="navbar-inner-right-profile-menu-bottom-list-item">
                                     <svg xmlns="http://www.w3.org/2000/svg"
                                          width="24px" height="24px"
@@ -515,6 +529,8 @@ const Navbar = ({ profile }) => {
                                     </svg>
                                     <span>Settings</span>
                                 </li>
+                            </ul>
+                            <ul className="navbar-inner-right-profile-menu-bottom-list list-1">
                                 <li className="navbar-inner-right-profile-menu-bottom-list-item-last"
                                     onClick={handleSignOut}>
                                     <svg xmlns="http://www.w3.org/2000/svg"
