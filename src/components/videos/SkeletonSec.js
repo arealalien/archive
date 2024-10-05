c5867cf3d76e412f96d90b1e40fe94fae0c0c795
Areal Alien ♡ <arild.edvin.andreassen@gmail.com>
2024-10-05 17:30:42 +0200

    added skeleton-loading to video sections

---
+++ src/components/videos/SkeletonSec.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const SkeletonSec = ({ count }) => {
+    const skeletonItems = Array.from({ length: count }, (_, index) => index);
+
+    return (
+        <>
+            {skeletonItems.map((_, index) => (
+                <div key={index} className="skeleton-inner-item">
+                    <div className="skeleton-inner-item-video"></div>
+                    <div className="skeleton-inner-item-info">
+                        <div className="skeleton-inner-item-bottom-info-left">
+                            <div className="skeleton-inner-item-info-left-image"></div>
+                        </div>
+                        <div className="skeleton-inner-item-info-right">
+                            <div className="skeleton-inner-item-info-right-title"></div>
+                            <div className="skeleton-inner-item-info-right-name"><span></span></div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default SkeletonSec;
--- src/components/videos/VideosSec.js
+++ src/components/videos/VideosSec.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { formatDistanceToNow } from "date-fns";
+import SkeletonSec from "./SkeletonSec";
 
 const VideosSec = ({ videoCreator, search, discovery }) => {
     const [videos, setVideos] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchVideos = async () => {
@@ -37,8 +39,10 @@ const VideosSec = ({ videoCreator, search, discovery }) => {
                     ...video,
                     duration: formatDuration(video.duration),
                 })));
+                setIsLoading(false);
             } catch (error) {
                 console.error('Error fetching video data:', error);
+                setIsLoading(false);
             }
         };
 
@@ -145,89 +149,91 @@ const VideosSec = ({ videoCreator, search, discovery }) => {
         thumbnail.style.opacity = "1";
     };
 
-
-
     return (
         <>
-            {videos.length > 0 ? (
-                videos.map((video, index) => (
-                    <div className="videos-inner-item" key={index} onMouseEnter={handleMouseEnter}
-                         onMouseLeave={handleMouseLeave}>
-                        <NavLink to={`/video?view=` + video.videoUrl.split('.')[0]} className="videos-inner-item-video">
-                            <div className="videos-inner-item-video-info">
-                                <div className="videos-inner-item-video-info-time">
-                                    <p>{video.duration}</p>
-                                    <div className="videos-inner-item-video-info-time-shadow"></div>
+            {isLoading ? (
+                <SkeletonSec count={10} />
+            ) : (
+                videos.length > 0 ? (
+                    videos.map((video, index) => (
+                        <div className="videos-inner-item" key={index} onMouseEnter={handleMouseEnter}
+                             onMouseLeave={handleMouseLeave}>
+                            <NavLink to={`/video?view=` + video.videoUrl.split('.')[0]} className="videos-inner-item-video">
+                                <div className="videos-inner-item-video-info">
+                                    <div className="videos-inner-item-video-info-time">
+                                        <p>{video.duration}</p>
+                                        <div className="videos-inner-item-video-info-time-shadow"></div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="videos-inner-item-info-line">
-                                <div className="videos-inner-item-info-line-progress">
-                                    <div className="videos-inner-item-info-line-progress-glow"></div>
+                                <div className="videos-inner-item-info-line">
+                                    <div className="videos-inner-item-info-line-progress">
+                                        <div className="videos-inner-item-info-line-progress-glow"></div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div to={`/video?view=` + video.videoUrl.split('.')[0]}
-                                 className="videos-inner-item-video-overlay"></div>
-                            <video
-                                className="videos-inner-item-video-video"
-                                src={process.env.PUBLIC_URL + "/users/" + video.creator.id + "/videos/" + video.videoUrl.split('.')[0] + "/" + video.videoUrl.split('.')[0] + "-144p." + video.videoUrl.split('.')[1]}
-                                muted
-                                loop
-                                preload="none"
-                            />
-                            <img className="videos-inner-item-video-background"
-                                 src={process.env.PUBLIC_URL + "/users/" + video.creator.id + "/videos/" + video.videoUrl.split('.')[0] + "/thumbnail.jpg"}
-                                 alt={video.title}/>
-                        </NavLink>
-                        <div className="videos-inner-item-info">
-                            <NavLink to={`/channel/${video.creator.name}`} className="videos-inner-item-bottom-info-left">
-                                <img className="videos-inner-item-info-left-image"
-                                     src={process.env.PUBLIC_URL + "/" + video.creator.profilePicture} alt=""/>
+                                <div to={`/video?view=` + video.videoUrl.split('.')[0]}
+                                     className="videos-inner-item-video-overlay"></div>
+                                <video
+                                    className="videos-inner-item-video-video"
+                                    src={process.env.PUBLIC_URL + "/users/" + video.creator.id + "/videos/" + video.videoUrl.split('.')[0] + "/" + video.videoUrl.split('.')[0] + "-144p." + video.videoUrl.split('.')[1]}
+                                    muted
+                                    loop
+                                    preload="none"
+                                />
+                                <img className="videos-inner-item-video-background"
+                                     src={process.env.PUBLIC_URL + "/users/" + video.creator.id + "/videos/" + video.videoUrl.split('.')[0] + "/thumbnail.jpg"}
+                                     alt={video.title}/>
                             </NavLink>
-                            <div className="videos-inner-item-info-right">
-                                <NavLink to={`/video?view=` + video.videoUrl.split('.')[0]}
-                                         className="videos-inner-item-info-right-title">{video.title}</NavLink>
-                                <NavLink to={`/channel/${video.creator.name}`}
-                                         className="videos-inner-item-info-right-name">
-                                    <span>{video.creator.displayName}</span>
-                                    {video.creator?.verified === 1 ? (
-                                        <svg className="verified-icon" viewBox="0 0 22 22" aria-hidden="true">
-                                            <g>
-                                                <linearGradient gradientUnits="userSpaceOnUse" id="a" x1="4.411"
-                                                                x2="18.083"
-                                                                y1="2.495" y2="21.508">
-                                                    <stop offset="0"></stop>
-                                                    <stop offset=".539"></stop>
-                                                    <stop offset=".68"></stop>
-                                                    <stop offset="1"></stop>
-                                                    <stop offset="1"></stop>
-                                                </linearGradient>
-                                                <linearGradient gradientUnits="userSpaceOnUse" id="b" x1="5.355"
-                                                                x2="16.361"
-                                                                y1="3.395" y2="19.133">
-                                                    <stop offset="0"></stop>
-                                                    <stop offset=".406"></stop>
-                                                    <stop offset=".989"></stop>
-                                                </linearGradient>
-                                                <g clip-rule="evenodd" fill-rule="evenodd">
-                                                    <path
-                                                        d="M13.324 3.848L11 1.6 8.676 3.848l-3.201-.453-.559 3.184L2.06 8.095 3.48 11l-1.42 2.904 2.856 1.516.559 3.184 3.201-.452L11 20.4l2.324-2.248 3.201.452.559-3.184 2.856-1.516L18.52 11l1.42-2.905-2.856-1.516-.559-3.184zm-7.09 7.575l3.428 3.428 5.683-6.206-1.347-1.247-4.4 4.795-2.072-2.072z"></path>
-                                                    <path
-                                                        d="M13.101 4.533L11 2.5 8.899 4.533l-2.895-.41-.505 2.88-2.583 1.37L4.2 11l-1.284 2.627 2.583 1.37.505 2.88 2.895-.41L11 19.5l2.101-2.033 2.895.41.505-2.88 2.583-1.37L17.8 11l1.284-2.627-2.583-1.37-.505-2.88zm-6.868 6.89l3.429 3.428 5.683-6.206-1.347-1.247-4.4 4.795-2.072-2.072z"></path>
-                                                    <path
-                                                        d="M6.233 11.423l3.429 3.428 5.65-6.17.038-.033-.005 1.398-5.683 6.206-3.429-3.429-.003-1.405.005.003z"></path>
-                                                </g>
-                                            </g>
-                                        </svg>
-                                    ) : null}
+                            <div className="videos-inner-item-info">
+                                <NavLink to={`/channel/${video.creator.name}`} className="videos-inner-item-bottom-info-left">
+                                    <img className="videos-inner-item-info-left-image"
+                                         src={process.env.PUBLIC_URL + "/" + video.creator.profilePicture} alt=""/>
                                 </NavLink>
-                                <NavLink to={`/video?view=` + video.videoUrl.split('.')[0]}
-                                         className="videos-inner-item-info-right-info">Seen {formatViews(video.views)} times &middot; {formatDistanceToNow(new Date(video.datePosted))} ago</NavLink>
+                                <div className="videos-inner-item-info-right">
+                                    <NavLink to={`/video?view=` + video.videoUrl.split('.')[0]}
+                                             className="videos-inner-item-info-right-title">{video.title}</NavLink>
+                                    <NavLink to={`/channel/${video.creator.name}`}
+                                             className="videos-inner-item-info-right-name">
+                                        <span>{video.creator.displayName}</span>
+                                        {video.creator?.verified === 1 ? (
+                                            <svg className="verified-icon" viewBox="0 0 22 22" aria-hidden="true">
+                                                <g>
+                                                    <linearGradient gradientUnits="userSpaceOnUse" id="a" x1="4.411"
+                                                                    x2="18.083"
+                                                                    y1="2.495" y2="21.508">
+                                                        <stop offset="0"></stop>
+                                                        <stop offset=".539"></stop>
+                                                        <stop offset=".68"></stop>
+                                                        <stop offset="1"></stop>
+                                                        <stop offset="1"></stop>
+                                                    </linearGradient>
+                                                    <linearGradient gradientUnits="userSpaceOnUse" id="b" x1="5.355"
+                                                                    x2="16.361"
+                                                                    y1="3.395" y2="19.133">
+                                                        <stop offset="0"></stop>
+                                                        <stop offset=".406"></stop>
+                                                        <stop offset=".989"></stop>
+                                                    </linearGradient>
+                                                    <g clip-rule="evenodd" fill-rule="evenodd">
+                                                        <path
+                                                            d="M13.324 3.848L11 1.6 8.676 3.848l-3.201-.453-.559 3.184L2.06 8.095 3.48 11l-1.42 2.904 2.856 1.516.559 3.184 3.201-.452L11 20.4l2.324-2.248 3.201.452.559-3.184 2.856-1.516L18.52 11l1.42-2.905-2.856-1.516-.559-3.184zm-7.09 7.575l3.428 3.428 5.683-6.206-1.347-1.247-4.4 4.795-2.072-2.072z"></path>
+                                                        <path
+                                                            d="M13.101 4.533L11 2.5 8.899 4.533l-2.895-.41-.505 2.88-2.583 1.37L4.2 11l-1.284 2.627 2.583 1.37.505 2.88 2.895-.41L11 19.5l2.101-2.033 2.895.41.505-2.88 2.583-1.37L17.8 11l1.284-2.627-2.583-1.37-.505-2.88zm-6.868 6.89l3.429 3.428 5.683-6.206-1.347-1.247-4.4 4.795-2.072-2.072z"></path>
+                                                        <path
+                                                            d="M6.233 11.423l3.429 3.428 5.65-6.17.038-.033-.005 1.398-5.683 6.206-3.429-3.429-.003-1.405.005.003z"></path>
+                                                    </g>
+                                                </g>
+                                            </svg>
+                                        ) : null}
+                                    </NavLink>
+                                    <NavLink to={`/video?view=` + video.videoUrl.split('.')[0]}
+                                             className="videos-inner-item-info-right-info">Seen {formatViews(video.views)} times &middot; {formatDistanceToNow(new Date(video.datePosted))} ago</NavLink>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))
-            ) : (
-                <p>No videos found</p>
+                    ))
+                ) : (
+                    <p>No videos found</p>
+                )
             )}
         </>
     );
